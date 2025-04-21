573f041ceb4997bd138b6450603083743a36b0f7
keskumar5 <keshavkumar.er97@gmail.com>
2025-04-21 23:34:49 +0530

    updated version

--- src/main/java/com/example/order_publisher/serdes/OrderSerializer.java
+++ src/main/java/com/example/order_publisher/serdes/OrderSerializer.java
@@ -2,13 +2,23 @@ package com.example.order_publisher.serdes;
 
 import com.example.order_publisher.model.StandingOrder;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.apache.kafka.common.serialization.Serializer;
 
 public class OrderSerializer implements Serializer<StandingOrder> {
+    private final ObjectMapper objectMapper;
+
+    public OrderSerializer() {
+        objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        this.objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+    }
+
     @Override
     public byte[] serialize(String topic, StandingOrder data) {
         try {
-            return new ObjectMapper().writeValueAsBytes(data);
+            return objectMapper.writeValueAsBytes(data);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
