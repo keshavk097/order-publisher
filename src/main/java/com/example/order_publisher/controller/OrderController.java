19a69a979e4308a43c66e4bf6c63ff7d20f91226
keskumar5 <keshavkumar.er97@gmail.com>
2025-04-22 13:56:09 +0530

    Added new Producer

--- src/main/java/com/example/order_publisher/controller/OrderController.java
+++ src/main/java/com/example/order_publisher/controller/OrderController.java
@@ -1,5 +1,6 @@
 package com.example.order_publisher.controller;
 
+import com.example.order_publisher.model.CustomerAccount;
 import com.example.order_publisher.model.StandingOrder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -9,18 +10,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/standing-orders")
+@RequestMapping("/api")
 public class OrderController {
 
     private final KafkaTemplate<String, StandingOrder> kafkaTemplate;
-
-    public OrderController(KafkaTemplate<String, StandingOrder> kafkaTemplate) {
+    private final KafkaTemplate<String, CustomerAccount> accountKafkaTemplate;
+    public OrderController(KafkaTemplate<String, StandingOrder> kafkaTemplate,KafkaTemplate<String, CustomerAccount> accountKafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
+        this.accountKafkaTemplate = accountKafkaTemplate;
     }
 
-    @PostMapping("/publish")
+    @PostMapping("/order")
     public ResponseEntity<String> publishOrder(@RequestBody StandingOrder order) {
         kafkaTemplate.send("orders-topic", order.getOrderId(),order);
+        return ResponseEntity.ok("Standing Order published successfully.");
+    }
+
+    @PostMapping("/account")
+    public ResponseEntity<?> sendAccount(@RequestBody CustomerAccount account) {
+        accountKafkaTemplate.send("customer-account-detail-topic", account.getCustomerId(),account);
         return ResponseEntity.ok("Standing Order published successfully.");
     }
 }
